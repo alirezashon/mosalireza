04e06db2bd97bbe436b7ef54006595d10ba3deda
Alireza <103531312+alirezashon@users.noreply.github.com>
2023-12-21 20:52:14 +0330

    selfectionmenu

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,16 @@ import Carouselali from '../Components/Carouselali'
 import Story from '../Components/Story'
 import Video from '../Components/video'
 import Hexadragong from '../Components/Hexadragong'
+import Navigation from '../Components/Navigation'
 const RootPage = () => {
 	return (
 		<>
+			<Navigation/>
 			<Personalinfo />
 			<Story />
 			<Carouselali />
 			<Hexadragong/>
 			<Video />
-			 
 		</>
 	)
 }
